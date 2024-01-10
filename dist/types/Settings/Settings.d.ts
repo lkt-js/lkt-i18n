17c908d416e4ef89f862fdcfdef7d2a73d5345d9
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2024-01-10 17:09:51 +0100

    Fix: typescript types bundle

---
+++ dist/types/Settings/Settings.d.ts
@@ -0,0 +1,8 @@
+export declare const Settings: {
+    NOT_FOUND_RETURN_EMPTY: number;
+    NOT_FOUND_RETURN_KEY: number;
+    value: {
+        notFoundReturnMode: number;
+        debugEnabled: boolean;
+    };
+};
---
+++ dist/types/index.d.ts
@@ -0,0 +1,6 @@
+import { LktObject } from "lkt-ts-interfaces";
+export declare const __: (key?: string, replacements?: {}) => any;
+export declare const setI18n: (i18n?: LktObject) => void;
+export declare const setI18nNotFoundReturnModeToEmpty: () => void;
+export declare const setI18nNotFoundReturnModeToKey: () => void;
+export declare const setI18nDebugMode: (status?: boolean) => void;
